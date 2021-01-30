4e359dd28046afc03b834ed89a8f06ae6aa1971f
David Vargas Hernandez <david-0610@live.com.mx>
2021-01-30 01:36:12 -0600

    Rest API

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -3,8 +3,22 @@ const router = Router();
 
 const Book = require("../models/Book");
 
-router.get("/api/books", (request, response) =>
-  response.json({ text: "Hello World" })
-);
+router.get("/api/books", async (req, res) => {
+  const books = await Book.find();
+  res.json(books);
+});
+
+router.post("/api/books", async (req, res) => {
+  const { title, author, isbn } = req.body;
+  const newBook = new Book({ title, author, isbn });
+  await newBook.save();
+  res.json({ message: "Book saved" });
+});
+
+router.delete("/api/books/:id", async (req, res) => {
+  const book = await Book.findByIdAndDelete(req.params.id);
+  console.log(book);
+  res.json({ message: "Book deleted" });
+});
 
 module.exports = router;
